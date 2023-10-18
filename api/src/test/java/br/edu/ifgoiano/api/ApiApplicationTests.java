8a8cbc4724d81069bbee404e104a05705239607d
Mateus de Paula Rezende <110778640+mateusp-rezende@users.noreply.github.com>
2023-10-17 23:15:23 -0300

    Add files via upload
    api rest

---
+++ api/src/test/java/br/edu/ifgoiano/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifgoiano.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
